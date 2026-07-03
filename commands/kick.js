bd4c803af2c2cf5a4a878443c174c49f3128d6dc
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:45:59 +0200

    Add kick command with user option and permissions

--- commands/kick.js
+++ commands/kick.js
@@ -1,1 +1,20 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("kick")
+    .setDescription("Kick utente")
+    .addUserOption(o =>
+      o.setName("user").setDescription("utente").setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const member = await interaction.guild.members.fetch(user.id);
+
+    await member.kick();
+
+    interaction.reply(`👢 ${user.tag} kickato`);
+  }
+};

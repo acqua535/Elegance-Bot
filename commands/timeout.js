584961b856f3e3eb5e84933a61d6bc0f95f66147
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 20:42:33 +0200

    Implement timeout command for users
    Adds a timeout command to put a user in timeout for a specified duration.

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,1 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("timeout")
+    .setDescription("Metti timeout a un utente")
+    .addUserOption(opt =>
+      opt.setName("user").setRequired(true)
+    )
+    .addIntegerOption(opt =>
+      opt.setName("minutes").setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const minutes = interaction.options.getInteger("minutes");
+
+    const member = await interaction.guild.members.fetch(user.id);
+
+    await member.timeout(minutes * 60 * 1000);
+
+    interaction.reply(`⏱️ ${user.tag} in timeout per ${minutes} min`);
+  }
+};

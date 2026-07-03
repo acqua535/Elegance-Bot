7482a91bf9bc53452c5019c010c470d691fa0983
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:45:35 +0200

    Add collab command with embed message

--- commands/collab.js
+++ commands/collab.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
+const COLLAB_CHANNEL = "1522610038831845518";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("collab")
+    .setDescription("Richiesta collab"),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle("⚡ Collab Request")
+      .setDescription(`Utente: ${interaction.user.tag}`)
+      .setColor("Purple");
+
+    const channel = interaction.guild.channels.cache.get(COLLAB_CHANNEL);
+    if (channel) channel.send({ embeds: [embed] });
+
+    interaction.reply({ content: "📨 collab inviata", ephemeral: true });
+  }
+};

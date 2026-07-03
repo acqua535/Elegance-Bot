ffb25ed8614b358f14515333c1124fd9d36237de
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 20:41:52 +0200

    Implement clear command for message deletion
    Adds a clear command to delete a specified number of messages.

--- commands/clear.js
+++ commands/clear.js
@@ -1,1 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Cancella messaggi")
+    .addIntegerOption(opt =>
+      opt.setName("amount")
+        .setDescription("Numero messaggi")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+  async execute(interaction) {
+    const amount = interaction.options.getInteger("amount");
+
+    await interaction.channel.bulkDelete(amount);
+
+    interaction.reply({
+      content: `🧹 Eliminati ${amount} messaggi`,
+      ephemeral: true
+    });
+  }
+};

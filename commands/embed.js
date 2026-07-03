4cc1becdc8e3b7037402a19aae93ad0a58499f17
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:51:50 +0200

    Implement embed command with text option
    Adds a new slash command to create an embed with specified text.

--- commands/embed.js
+++ commands/embed.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("Crea un embed")
+    .addStringOption(o =>
+      o.setName("text").setDescription("contenuto").setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+  async execute(interaction) {
+    const text = interaction.options.getString("text");
+
+    const embed = new EmbedBuilder()
+      .setDescription(text)
+      .setColor("Blue");
+
+    interaction.reply({ embeds: [embed] });
+  }
+};

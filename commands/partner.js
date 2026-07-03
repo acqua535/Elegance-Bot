fddcd3123ab1a88aff2963485452bb6afe6d7bf7
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:45:10 +0200

    Add partner command with embed message

--- commands/partner.js
+++ commands/partner.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
+const PARTNER_CHANNEL = "1508774443286003815";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("partner")
+    .setDescription("Richiesta partner"),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle("🤝 Partner Request")
+      .setDescription(`Utente: ${interaction.user.tag}`)
+      .setColor("Blue");
+
+    const channel = interaction.guild.channels.cache.get(PARTNER_CHANNEL);
+    if (channel) channel.send({ embeds: [embed] });
+
+    interaction.reply({ content: "📨 richiesta inviata", ephemeral: true });
+  }
+};

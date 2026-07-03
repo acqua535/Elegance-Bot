a852871b657daa37eddce42831ea0104abd111ec
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 17:00:58 +0200

    Add load and save functions for leaderboard data

--- utils/lbHelper.js
+++ utils/lbHelper.js
@@ -1,1 +1,14 @@
+const fs = require("fs");
 
+const file = "./utils/leaderboard.json";
+
+function load() {
+  if (!fs.existsSync(file)) fs.writeFileSync(file, "{}");
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function save(data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = { load, save };

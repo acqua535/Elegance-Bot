94d3b182f6af647bd85d79d391ac85361ca2a569
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:44:42 +0200

    Add meme command using SlashCommandBuilder

--- commands/meme.js
+++ commands/meme.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("meme")
+    .setDescription("Meme casuale"),
+
+  async execute(interaction) {
+    try {
+      const res = await fetch("https://meme-api.com/gimme");
+      const data = await res.json();
+
+      interaction.reply({
+        content: `${data.title}\n${data.url}`
+      });
+
+    } catch (err) {
+      interaction.reply("❌ Errore nel caricamento meme");
+    }
+  }
+};

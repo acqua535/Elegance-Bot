75eeed574a5f1bffc13936ad570757cb8eb7775e
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 16:48:26 +0200

    Add userinfo command with user option

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("userinfo")
+    .setDescription("Info utente")
+    .addUserOption(o =>
+      o.setName("user").setDescription("utente").setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user") || interaction.user;
+
+    interaction.reply(`👤 ${user.tag}\n🆔 ${user.id}`);
+  }
+};

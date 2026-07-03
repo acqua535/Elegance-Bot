72e93e221b5e28c3961411770d88ed8c087db828
acqua535 <marcobevilacqua154@gmail.com>
2026-07-03 20:42:07 +0200

    Add ban command for banning users

--- commands/ban.js
+++ commands/ban.js
@@ -1,1 +1,22 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Banna un utente")
+    .addUserOption(opt =>
+      opt.setName("user")
+        .setDescription("Utente da bannare")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const member = await interaction.guild.members.fetch(user.id);
+
+    await member.ban();
+
+    interaction.reply(`🔨 ${user.tag} bannato`);
+  }
+};

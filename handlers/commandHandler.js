63a5e19c6cc6a7ad7f383b483a98662adb787a4e
acqua535 <marcobevilacqua154@gmail.com>
2026-07-12 19:26:48 +0200

    Update command file filtering logic

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -3,7 +3,12 @@ const fs = require("fs");
 module.exports = (client) => {
 
     const commandFiles = fs.readdirSync(__dirname)
-        .filter(file => file === "say.js");
+        .filter(file =>
+            file.endsWith(".js") &&
+            file !== "index.js" &&
+            file !== "commandHandler.js" &&
+            file !== "deployCommands.js"
+        );
 
     for (const file of commandFiles) {
         const command = require(`./${file}`);

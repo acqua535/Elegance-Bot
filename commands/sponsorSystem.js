cb6b831002864adb14919551dada9d0d3fe0078a
acqua535 <marcobevilacqua154@gmail.com>
2026-07-12 20:57:22 +0200

    Create sponsorSystem.js

---
+++ commands/sponsorSystem.js
@@ -0,0 +1,90 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const { sendPartnership } = require("../partnerSystem");
+
+
+module.exports = {
+
+
+    data: new SlashCommandBuilder()
+
+        .setName("sponsor")
+
+        .setDescription("Invia un messaggio sponsor")
+
+
+        .addStringOption(option =>
+
+            option
+
+                .setName("descrizione")
+
+                .setDescription("Descrizione dello sponsor")
+
+                .setRequired(true)
+
+        )
+
+
+        .addRoleOption(option =>
+
+            option
+
+                .setName("ping")
+
+                .setDescription("Ruolo da menzionare")
+
+                .setRequired(false)
+
+        )
+
+
+        .addUserOption(option =>
+
+            option
+
+                .setName("manager")
+
+                .setDescription("Manager dello sponsor")
+
+                .setRequired(false)
+
+        )
+
+
+        .addStringOption(option =>
+
+            option
+
+                .setName("data")
+
+                .setDescription("Data dello sponsor")
+
+                .setRequired(false)
+
+        ),
+
+
+
+    async execute(interaction) {
+
+
+        await sendPartnership(
+
+            interaction,
+
+            "1521856540016115803",
+
+            "Sponsor",
+
+            "⚜️",
+
+            "#f1c40f"
+
+        );
+
+
+    }
+
+
+};

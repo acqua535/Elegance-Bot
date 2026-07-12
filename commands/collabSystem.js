812bc846f7ad3a1376349d2d4796f8243487468d
acqua535 <marcobevilacqua154@gmail.com>
2026-07-12 20:56:42 +0200

    Create collabSystem.js

---
+++ commands/collabSystem.js
@@ -0,0 +1,90 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const { sendPartnership } = require("../partnerSystem");
+
+
+module.exports = {
+
+
+    data: new SlashCommandBuilder()
+
+        .setName("collab")
+
+        .setDescription("Invia una collaborazione")
+
+
+        .addStringOption(option =>
+
+            option
+
+                .setName("descrizione")
+
+                .setDescription("Descrizione della collaborazione")
+
+                .setRequired(true)
+
+        )
+
+
+        .addRoleOption(option =>
+
+            option
+
+                .setName("ping")
+
+                .setDescription("Ruolo da menzionare")
+
+                .setRequired(false)
+
+        )
+
+
+        .addUserOption(option =>
+
+            option
+
+                .setName("manager")
+
+                .setDescription("Manager della collaborazione")
+
+                .setRequired(false)
+
+        )
+
+
+        .addStringOption(option =>
+
+            option
+
+                .setName("data")
+
+                .setDescription("Data della collaborazione")
+
+                .setRequired(false)
+
+        ),
+
+
+
+    async execute(interaction) {
+
+
+        await sendPartnership(
+
+            interaction,
+
+            "1508774443286003815",
+
+            "Collab",
+
+            "🤝",
+
+            "#3498db"
+
+        );
+
+
+    }
+
+
+};

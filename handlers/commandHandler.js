6af6a089cb3e73eda4ed20a812a51845631223e0
acqua535 <marcobevilacqua154@gmail.com>
2026-07-12 18:58:38 +0200

    Refactor command loading to use local file

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
-const path = require("path");
 
 module.exports = (client) => {
-    const commandsPath = path.join(__dirname, "../commands");
 
-    if (!fs.existsSync(commandsPath)) {
-        fs.mkdirSync(commandsPath);
-    }
-
-    const commandFiles = fs.readdirSync(commandsPath)
-        .filter(file => file.endsWith(".js"));
+    const commandFiles = fs.readdirSync(__dirname)
+        .filter(file => file === "say.js");
 
     for (const file of commandFiles) {
-        const command = require(`${commandsPath}/${file}`);
+        const command = require(`./${file}`);
 
         if (command.data && command.execute) {
             client.commands.set(command.data.name, command);
+
             console.log(`✅ Comando caricato: ${command.data.name}`);
         }
     }

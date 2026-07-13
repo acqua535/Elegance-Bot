c8266234b55e3a3376fc79ee5b7053aefee41b13
acqua535 <marcobevilacqua154@gmail.com>
2026-07-13 03:23:04 +0200

    Create moderationSystem.js

---
+++ moderationSystem.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+
+const file = "./warnings.json";
+
+
+function loadWarnings() {
+
+    if (!fs.existsSync(file)) {
+
+        fs.writeFileSync(
+            file,
+            "{}"
+        );
+
+    }
+
+
+    return JSON.parse(
+        fs.readFileSync(file, "utf8")
+    );
+
+}
+
+
+
+function saveWarnings(data) {
+
+    fs.writeFileSync(
+        file,
+        JSON.stringify(data, null, 4)
+    );
+
+}
+
+
+
+function addWarning(userId, moderatorId, reason) {
+
+    const data = loadWarnings();
+
+
+    if (!data[userId]) {
+
+        data[userId] = [];
+
+    }
+
+
+
+    data[userId].push({
+
+        moderator: moderatorId,
+
+        reason: reason,
+
+        date: Date.now()
+
+    });
+
+
+
+    saveWarnings(data);
+
+
+    return data[userId].length;
+
+}
+
+
+
+function getWarnings(userId) {
+
+    const data = loadWarnings();
+
+
+    return data[userId] || [];
+
+}
+
+
+
+function clearWarnings(userId) {
+
+    const data = loadWarnings();
+
+
+    delete data[userId];
+
+
+    saveWarnings(data);
+
+}
+
+
+
+module.exports = {
+
+    addWarning,
+
+    getWarnings,
+
+    clearWarnings
+
+};

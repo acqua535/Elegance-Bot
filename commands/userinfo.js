ae3ecb02d930ccea26f02c511a9cb64523263454
acqua535 <marcobevilacqua154@gmail.com>
2026-07-13 07:40:24 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,15 +7,50 @@ const fs = require("fs");
 const path = require("path");
 
 
+
 const FILE =
 path.join(
     __dirname,
-    "../utenti.json"
+    "../users.json"
 );
 
 
 
+
+function loadUsers(){
+
+
+    if(!fs.existsSync(FILE)){
+
+
+        fs.writeFileSync(
+            FILE,
+            "[]"
+        );
+
+
+    }
+
+
+
+    return JSON.parse(
+
+        fs.readFileSync(
+            FILE,
+            "utf8"
+        )
+
+    );
+
+
+}
+
+
+
+
+
 module.exports = {
+
 
 
 data:
@@ -25,28 +60,37 @@ new SlashCommandBuilder()
 .setName("userinfo")
 
 .setDescription(
-    "Mostra informazioni di un utente"
+"Mostra informazioni di un utente"
 )
+
 
 .addUserOption(option =>
 
-    option
-    .setName("utente")
-    .setDescription(
-        "Utente da controllare"
-    )
-    .setRequired(false)
+option
+
+.setName("utente")
+
+.setDescription(
+"Utente da controllare"
+)
+
+.setRequired(false)
 
 ),
+
+
 
 
 
 async execute(interaction){
 
 
+
 const user =
 
-interaction.options.getUser("utente")
+interaction.options.getUser(
+"utente"
+)
 ||
 interaction.user;
 
@@ -54,15 +98,17 @@ interaction.user;
 
 const member =
 
-await interaction.guild.members
-.fetch(user.id)
+await interaction.guild.members.fetch(
+user.id
+)
 .catch(
-    () => null
+()=>null
 );
 
 
 
 if(!member){
+
 
 return interaction.reply({
 
@@ -73,35 +119,28 @@ ephemeral:true
 
 });
 
-}
-
-
-
-let users = [];
-
-
-
-if(fs.existsSync(FILE)){
-
-users =
-
-JSON.parse(
-fs.readFileSync(
-FILE,
-"utf8"
-)
-);
 
 }
+
+
+
+
+
+const users =
+loadUsers();
 
 
 
 const userData =
 
 users.find(
+
 u =>
 u.id === user.id
+
 );
+
+
 
 
 
@@ -113,25 +152,26 @@ userData?.birthday
 
 
 
-// Ruoli
+
+
+
 
 const roles =
 
 member.roles.cache
 
 .filter(
+
 role =>
 role.id !== interaction.guild.id
-)
 
-.sort(
-(a,b)=>
-b.position - a.position
 )
 
 .map(
+
 role =>
 role.toString()
+
 )
 
 .join("\n")
@@ -140,31 +180,17 @@ role.toString()
 
 
 
-const roleCount =
-
-member.roles.cache.size - 1;
 
 
 
-const highestRole =
-
-member.roles.highest.id !== interaction.guild.id
-
-?
-
-member.roles.highest.toString()
-
-:
-
-"Nessun ruolo";
-
-
-
-// Date
 
 const created =
 
-`<t:${Math.floor(user.createdTimestamp / 1000)}:D>`;
+`<t:${Math.floor(
+user.createdTimestamp / 1000
+)}:D>`;
+
+
 
 
 
@@ -174,11 +200,15 @@ member.joinedTimestamp
 
 ?
 
-`<t:${Math.floor(member.joinedTimestamp / 1000)}:D>`
+`<t:${Math.floor(
+member.joinedTimestamp / 1000
+)}:D>`
 
 :
 
 "Non disponibile";
+
+
 
 
 
@@ -188,11 +218,16 @@ member.joinedTimestamp
 
 ?
 
-`<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`
+`<t:${Math.floor(
+member.joinedTimestamp / 1000
+)}:R>`
 
 :
 
 "Non disponibile";
+
+
+
 
 
 
@@ -203,89 +238,144 @@ const embed =
 
 new EmbedBuilder()
 
+
+
 .setTitle(
+
 "⚜️ USER INFO"
+
 )
 
+
+
 .setThumbnail(
+
 user.displayAvatarURL({
+
 dynamic:true,
+
 size:1024
+
 })
+
 )
+
 
 
 .addFields(
 
 
 {
-name:"👤 Utente",
+
+name:
+"👤 Utente",
+
 value:
 `${user}`
+
 },
 
 
+
 {
-name:"🆔 ID",
+
+name:
+"🆔 ID",
+
 value:
 user.id
+
 },
 
 
+
 {
-name:"📅 Account creato",
+
+name:
+"🤖 Tipo",
+
+value:
+user.bot
+?
+"Bot"
+:
+"Utente"
+
+},
+
+
+
+{
+
+name:
+"📅 Account creato",
+
 value:
 created
+
 },
 
 
+
 {
-name:"📥 Entrato nel server",
+
+name:
+"📥 Entrato nel server",
+
 value:
 joined
+
 },
 
 
+
 {
-name:"⏳ Nel server da",
+
+name:
+"⏳ Nel server da",
+
 value:
 joinedRelative
+
 },
 
 
+
 {
-name:"👑 Ruolo più alto",
+
+name:
+"🎭 Ruoli",
+
 value:
-highestRole
+roles.substring(
+0,
+1024
+)
+
 },
 
 
-{
-name:"🎭 Numero ruoli",
-value:
-`${roleCount}`
-},
-
 
 {
-name:"🎭 Ruoli",
-value:
-roles.substring(0,1024)
-},
 
+name:
+"🎂 Compleanno",
 
-{
-name:"🎂 Compleanno",
 value:
 birthday
+
 }
 
+
+
 )
+
 
 
 .setColor(
 0x5865F2
 )
+
 
 
 .setFooter({
@@ -296,20 +386,28 @@ text:
 })
 
 
+
 .setTimestamp();
+
+
+
 
 
 
 await interaction.reply({
 
 embeds:[
+
 embed
+
 ]
 
 });
 
 
+
 }
+
 
 
 };

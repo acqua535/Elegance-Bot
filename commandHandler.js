8fc20ee3a503d94fc1fa657faabfad63b11dcedf
acqua535 <marcobevilacqua154@gmail.com>
2026-07-20 04:39:01 +0200

    Update commandHandler.js

--- commandHandler.js
+++ commandHandler.js
@@ -9,16 +9,16 @@ module.exports = function loadCommands(client) {
     const rootPath = process.cwd();
     const commandsPath = path.join(rootPath, "commands");
     
-    // Lista dei file da caricare in locale
+    // Lista completa dei file inclusi i ticket
     const targetCommands = [
         "collab.js", "daily-reward.js", "embed.js", "horror.js", 
         "minigame.js", "modify-suggest.js", "partner.js", "say.js", 
-        "sponsor.js", "suggest.js", "unwarn.js", "warn.js", "warnings.js"
+        "sponsor.js", "suggest.js", "ticket.js", "unwarn.js", "warn.js", "warnings.js"
     ];
 
     const filesToLoad = new Map();
 
-    // 1. Controlla in 'commands'
+    // 1. Controlla nella cartella 'commands'
     if (fs.existsSync(commandsPath)) {
         const internalItems = fs.readdirSync(commandsPath);
         internalItems.forEach(file => {
@@ -36,7 +36,9 @@ module.exports = function loadCommands(client) {
         }
     });
 
-    // 3. Carica i comandi trovati
+    console.log(`🔎 [DEBUG] Trovati ${filesToLoad.size} comandi totali distribuiti nel server.`);
+
+    // 3. Carica i comandi in memoria locale
     for (const [file, filePath] of filesToLoad.entries()) {
         try {
             const command = require(filePath);

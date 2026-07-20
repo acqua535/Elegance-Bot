10b112c36e16ed91d10bd4333572a08e166a92f0
acqua535 <marcobevilacqua154@gmail.com>
2026-07-20 05:01:49 +0200

    Update unwarn.js

--- commands/unwarn.js
+++ commands/unwarn.js
@@ -1,31 +1,17 @@
-const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    PermissionFlagsBits
-} = require("discord.js");
-
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const moderation = require("./moderationSystem");
 
-
 module.exports = {
-
     data: new SlashCommandBuilder()
-
         .setName("unwarn")
-
         .setDescription("Rimuove un warn da un utente")
-
-        .setDefaultMemberPermissions(
-            PermissionFlagsBits.ModerateMembers
-        )
-
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
         .addUserOption(option =>
             option
                 .setName("utente")
                 .setDescription("Utente")
                 .setRequired(true)
         )
-
         .addIntegerOption(option =>
             option
                 .setName("numero")
@@ -33,96 +19,39 @@ module.exports = {
                 .setRequired(true)
         ),
 
-
-
     async execute(interaction) {
-
-
         const user = interaction.options.getUser("utente");
-
         const number = interaction.options.getInteger("numero");
-
-
-
         const warnings = moderation.getWarnings(user.id);
 
-
-
         if (warnings.length === 0) {
-
             return interaction.reply({
-
                 content: "❌ Questo utente non ha warn.",
-
                 ephemeral: true
-
             });
-
         }
-
-
 
         if (number < 1 || number > warnings.length) {
-
             return interaction.reply({
-
                 content: `❌ Inserisci un numero da 1 a ${warnings.length}.`,
-
                 ephemeral: true
-
             });
-
         }
 
-
-
-        const removed = warnings.splice(
-            number - 1,
-            1
-        );
-
-
-
-        moderation.updateWarnings(
-            user.id,
-            warnings
-        );
-
-
+        const removed = warnings.splice(number - 1, 1);
+        moderation.updateWarnings(user.id, warnings);
 
         const embed = new EmbedBuilder()
-
             .setTitle("✅ Warn rimosso")
-
             .setColor("Green")
-
             .setDescription(
-
-`👤 **Utente**
-${user}
-
-🛡️ **Rimosso da**
-${interaction.user}
-
-📝 **Motivo rimosso**
-${removed[0].reason}
-
-📊 **Warn rimasti**
-${warnings.length}`
-
+                `👤 **Utente**\n${user}\n\n` +
+                `🛡️ **Rimosso da**\n${interaction.user}\n\n` +
+                `📝 **Motivo rimosso**\n${removed[0].reason}\n\n` +
+                `📊 **Warn rimasti**\n${warnings.length}`
             )
-
             .setTimestamp();
 
-
-
-        await interaction.reply({
-
-            embeds: [embed]
-
-        });
-
-
+        await interaction.reply({ embeds: [embed] });
     }
-
 };

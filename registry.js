448a4ecc8546e2cda5eb354143c12b453a7934ab
acqua535 <marcobevilacqua154@gmail.com>
2026-07-20 19:20:57 +0200

    Refactor ticket and game handler exports
    Refactor ticket and game handlers for clarity.

--- registry.js
+++ registry.js
@@ -2,21 +2,19 @@ const ticket = require("./ticket");
 const games = require("./minigame");
 
 module.exports = {
-    // Ticket System
+    // Ticket
+    "ticket_category": ticket.categoryHandler,
     "ticket_manage": ticket.buttonHandler,
     "claim_ticket": ticket.buttonHandler,
     "close_ticket": ticket.buttonHandler,
     "ping_staff": ticket.buttonHandler,
-    "ticket_category": ticket.categoryHandler,
-
-    // Minigame Hub & Games
-    "game_quiz": (i) => games.quizGame(i),
-    "game_memory": (i) => games.memoryGame(i),
-    "game_word": (i) => games.wordGame(i),
-    "game_reaction": (i) => games.reactionGame(i),
-    "game_hangman": (i) => games.hangmanGame(i),
-
-    // Difficoltà Parola Misteriosa
+    
+    // Giochi
+    "game_quiz": games.quizGame,
+    "game_memory": games.memoryGame,
+    "game_word": games.wordGame,
+    "game_reaction": games.reactionGame,
+    "game_hangman": games.hangmanGame,
     "word_easy": (i) => games.startWordGame(i, "facile"),
     "word_medium": (i) => games.startWordGame(i, "medio"),
     "word_hard": (i) => games.startWordGame(i, "difficile")

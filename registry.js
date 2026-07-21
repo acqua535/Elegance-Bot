2ba51d6eb38d12db4380f0a138ae69add1124e16
acqua535 <marcobevilacqua154@gmail.com>
2026-07-21 22:24:21 +0200

    Update registry.js

--- registry.js
+++ registry.js
@@ -1,16 +1,22 @@
 const ticket = require("./ticket");
 const games = require("./minigame");
 const verify = require("./verify");
+const entry = require("./entry");
 
 module.exports = {
-    // Ticket
+    // --- TICKET ---
     "ticket_category": ticket.categoryHandler,
     "ticket_manage": ticket.buttonHandler,
     "claim_ticket": ticket.buttonHandler,
     "close_ticket": ticket.buttonHandler,
     "ping_staff": ticket.buttonHandler,
     
-    // Giochi
+    // --- RATING TICKET ---
+    "rate_good": ticket.ratingHandler,
+    "rate_mid": ticket.ratingHandler,
+    "rate_bad": ticket.ratingHandler,
+
+    // --- GIOCHI ---
     "game_quiz": games.quizGame,
     "game_memory": games.memoryGame,
     "game_word": games.wordGame,
@@ -20,12 +26,12 @@ module.exports = {
     "word_medium": (i) => games.startWordGame(i, "medio"),
     "word_hard": (i) => games.startWordGame(i, "difficile"),
 
-    // Rating (Aggiunti qui dentro)
-    "rate_good": ticket.ratingHandler,
-    "rate_mid": ticket.ratingHandler,
-    "rate_bad": ticket.ratingHandler,
-
-    // Sistema di Verifica
+    // --- VERIFICA / CAPTCHA ---
     "verify_button": verify.buttonHandler,
-    "verify_modal": verify.modalHandler
+    "verify_modal": verify.modalHandler,
+
+    // --- BENVENUTO / ADDIO (ENTRY SYSTEM) ---
+    "entry_toggle_welcome": entry.buttonHandler,
+    "entry_toggle_leave": entry.buttonHandler,
+    "entry_set_channel": entry.buttonHandler
 };
